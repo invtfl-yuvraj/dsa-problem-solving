782f480c4484eb3f317d9900424e4b669e9ecf5b
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2024-04-11 23:56:29 +0530

    Solved Leetcode - Task Scheduler problem

---
+++ LeetCode/33 - 19:03:2024 Task Scheduler/solution.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+
+        vector<int> frequencies(26, 0);
+        for (char task : tasks) {
+            frequencies[task - 'A']++;
+        }
+        
+        sort(frequencies.begin(), frequencies.end(), greater<int>());
+        
+        int maxFreq = frequencies[0];
+        
+        int idleTime = (maxFreq - 1) * n;
+        for (int i = 1; i < frequencies.size() && frequencies[i] > 0; i++) {
+            idleTime -= min(frequencies[i], maxFreq - 1);
+        }
+        
+        idleTime = max(0, idleTime);
+        return tasks.size() + idleTime;
+    }
+};
