b81148788a85ec62f4b878729f2388167a5b47fc
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2024-04-18 23:32:04 +0530

    Made changes in solution

--- GeekForGeeks/44 - 21:03:2024 Move all negative elements to end/solution.cpp
+++ GeekForGeeks/44 - 21:03:2024 Move all negative elements to end/solution.cpp
@@ -6,7 +6,7 @@ class Solution{
     public:
     void segregateElements(int arr[],int n)
     {
-       int arr2[n] = {};
+       int arr2[n];
        
        int k = 0;
        for (int i = 0; i< n; i++){
