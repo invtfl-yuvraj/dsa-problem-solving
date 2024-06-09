d73f28fcf1e462c6fd71c6d6434656e38efdd7b7
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2024-06-09 23:09:15 +0530

    Solved GeeksforGeeks - Convert array into ZIg-Zag fashion problem and added the solution file

---
+++ GeekForGeeks/09:06:2024 Convert array into Zig-Zag fashion/solution.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    void zigZag(int n, vector<int> &arr) {
+        // code here
+        
+        for (int i = 0; i < n-1; i++){
+            if (i%2 ==0){
+                if (arr[i] > arr[i+1]){
+                    swap(arr[i],arr[i+1]);
+                }
+            }
+            else {
+                if (arr[i] < arr[i+1]){
+                    swap(arr[i],arr[i+1]);
+                }
+            }
+        }
+    }
+};
