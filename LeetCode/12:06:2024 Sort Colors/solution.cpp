a798a0b41be897bb5fdb9398342175e4618dbfa0
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2024-06-12 23:52:30 +0530

    Solved LeetCode - Sort Colors POTD problem and added the solution file

---
+++ LeetCode/12:06:2024 Sort Colors/solution.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = 0;
+
+        while (mid <= right) {
+            if (nums[mid] == 0) {
+                swap(nums[mid], nums[left]);
+                mid++;
+                left++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[right]);
+                right--;
+            }
+        }
+    }
+};
