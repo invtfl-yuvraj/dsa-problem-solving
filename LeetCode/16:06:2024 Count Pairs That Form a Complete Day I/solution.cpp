5db582836946e7f740f5b9600e296c9d6e5f5d26
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2024-06-17 23:45:08 +0530

    Solved Weekly contest Leetcode - Count Pairs that form a complete day I problem and added the solution file

---
+++ LeetCode/16:06:2024 Count Pairs That Form a Complete Day I/solution.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& hours) {
+        
+        int count = 0;
+        for (int i = 0; i < hours.size()-1; i++){
+            for (int j = i+1; j <hours.size(); j++){
+                if ((hours[i] + hours[j]) % 24 == 0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
