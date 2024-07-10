ccdacfc1137f695446770984623159ba0339c97a
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2024-07-10 21:30:44 +0530

    Solved LeetCode - Richest Customer wealth problem

---
+++ LeetCode/09:07:2024 Richest Customer Wealth/solution.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxi = 0;
+        for(int i = 0; i < accounts.size(); i++) {
+            int sum = 0;
+            for(int j = 0; j < accounts[i].size(); j++) {
+                  sum += accounts[i][j];
+            }
+            maxi=max(maxi,sum);
+        }
+        return maxi;
+
+    }
+};
