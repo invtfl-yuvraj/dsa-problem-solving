182600285bee950217de5eb052af1674816e2271
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2024-07-21 23:56:07 +0530

    Solved GeeksforGeeks - Maximum indentical bowls problem and added the solution file

---
+++ GeekForGeeks/21:07:2024 Maximum Identical Bowls/solution.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    int getMaximum(int N, vector<int> &arr) {
+        // code here
+        long long int sum = 0;
+        for(int i=0; i<N; i++)
+            sum += arr[i];
+            
+        while(N)
+            if(sum % N-- == 0)
+            return N + 1;
+    }
+};
