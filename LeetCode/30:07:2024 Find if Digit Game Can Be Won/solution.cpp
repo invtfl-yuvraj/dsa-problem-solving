4cf755f73f587ff699691fd478a27522b9ec1737
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2024-07-30 23:38:28 +0530

    Solved LeetCode - Find if Digit Game can be won and added the solution file

---
+++ LeetCode/30:07:2024 Find if Digit Game Can Be Won/solution.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int sum1 = 0;
+        int sum2 = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 10) {
+                sum1 += nums[i];
+            } else {
+                sum2 += nums[i];
+            }
+        }
+        if (sum1 == sum2)
+            return false;
+        else {
+            return true;
+        }
+    }
+};
