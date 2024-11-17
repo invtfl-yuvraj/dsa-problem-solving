145614db298e1a1ec3da670d3cb26c8c3a1c3db0
Yuvraj Singh <152969258+invtfl-yuvraj@users.noreply.github.com>
2024-11-17 23:32:03 +0530

    Create 17-11-24-rEVERSE-aRRAY.cpp

---
+++ GeekForGeeks/17-11-24-rEVERSE-aRRAY/17-11-24-rEVERSE-aRRAY.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int>result(n);
+        for(int i = 0; i < n; i++) {
+            result[i] = arr[n - 1 - i];
+            
+        }
+        
+        for(int i = 0; i < n; i++) {
+            arr[i] = result[i];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
