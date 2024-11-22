8408df2c40f5e443643a0c4ec169badd79e94a3b
Yuvraj Singh <152969258+invtfl-yuvraj@users.noreply.github.com>
2024-11-22 22:04:49 +0530

    Create 22-11-24-Stock Buy and Sell – Max one Transaction Allowed.cpp

---
+++ GeekForGeeks/22-11-24-Stock Buy and Sell – Max one Transaction Allowed/22-11-24-Stock Buy and Sell – Max one Transaction Allowed.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int n = prices.size();
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        for(int i = 0; i  < n; i++) {
+            minPrice = min(minPrice, prices[i]);
+            int profit = prices[i] - minPrice;
+            maxProfit = max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> prices;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            prices.push_back(number);
+        }
+
+        Solution ob;
+        int ans = ob.maximumProfit(prices);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
